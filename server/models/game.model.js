1bc71059e552542355c4c3512044b6b95a27f2cb
Acearoni <awoodson.contact@gmail.com>
2025-01-15 21:46:26 -0600

    validations added to backend.

--- server/models/game.model.js
+++ server/models/game.model.js
@@ -3,10 +3,28 @@ const mongoose = require('mongoose'); //importing Mongoose Library
 
 //This is a blueprint of how our data should look.
 const GameSchema = mongoose.Schema({ //mongoose.Schema() is a function, then add the {} into it, passing in an object.
-    gameName: String,
-    dev: String,
-    releaseYear: Number,
-    genre: String
+    gameName: {
+        type: String,
+        required: [true, "Game Name is required."],
+        minLength: [3, "Game Name must be 3 characters long."],
+        maxLength: [30, "Game Name is too long."]
+    },
+    dev: {
+        type: String,
+        required: [true, "Developer Name is required."],
+        minLength: [3, "Developer Name must be 3 characters long."]
+    },
+    releaseYear:{
+        type: Number,
+        required: [true, "A Release Year is required."],
+        min:[1960, "Release Year must be at least 1960 or later."],
+        max:[2025, "Release Year cannot be past 2025."]
+    },
+    genre: {
+        type: String,
+        required: [true, "A Genre is required."],
+        minLength: [3, "Genre must be 3 characters long."]
+    }
 
     //makes createdAt and updatedAt work for us behind the scenes
 } , {timestamps: true} )  
